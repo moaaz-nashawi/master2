9ac903e9706d51a7bc0eb8ca5247386a9ba0e1dc
Moaaz Nashawi <m.nashawi@anchormen.nl>
2022-12-19 14:52:17 +0100

    p

---
+++ src/main/java/com/example/master2/Master2Application.java
@@ -0,0 +1,14 @@
+package com.example.master2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Master2Application {
+
+    public static void main(String[] args) {
+        //
+        SpringApplication.run(Master2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/master2/Master2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.master2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Master2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
